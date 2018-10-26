6984e136f8bf7f05ff5ca81e50b75890de4ed53b
Syed <syed@codeplaylabs.com>
2018-10-26 20:00:34 +0530

    commit init

---
+++ Riztokit/src/main/java/com/shaistech/riztokit/Parser.java
@@ -0,0 +1,39 @@
+package com.shaistech.riztokit;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * Created By Syed Hussain Medhi
+ * email : shmehdi01@gmail.com
+ * youtube : youtube.com/shmehdi5
+ * */
+
+public abstract class Parser<T>{
+
+   protected Gson gson;
+
+   public Parser(){
+       gson = new Gson();
+   }
+
+
+    /**
+     * This method is actually parsing the json using {@link Gson}
+     */
+
+    protected T parse(String s){
+        if(parseJson() != null) return gson.fromJson(s,parseJson().getType()); return null;
+    }
+
+    /**
+     * This method will parse your json request {@link Response}
+     * The return type of this method is TypeToken with your model class
+     * This method will be implemented and you have to return new TypeToke
+     *
+     * e.g return new TypeToken<YourModel>(){};
+     * */
+
+    protected abstract TypeToken<T> parseJson();
+
+}
---
+++ Riztokit/src/main/java/com/shaistech/riztokit/Response.java
@@ -0,0 +1,20 @@
+package com.shaistech.riztokit;
+
+/**
+ * Created By Syed Hussain Medhi
+ * email : shmehdi01@gmail.com
+ * youtube : youtube.com/shmehdi5
+ * */
+
+public interface Response<Model> {
+
+    /**
+     * @Override this method where your are using {@link Service}
+     * This method will return a response of your request
+     * model is a model class you passing in generic {@link Service} and {@link Parser}
+     * json is your response json
+     * url is your request url
+     * You need to null check if you are passing {@link Parser} as null in {@link Service} model will be null.
+     * */
+    public void onServerResponse(Model model, String json, String url);
+}
