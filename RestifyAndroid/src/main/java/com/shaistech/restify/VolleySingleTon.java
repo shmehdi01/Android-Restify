3d2ee82b566560c68b5f43b84b9a88b33837057f
Syed <syed@codeplaylabs.com>
2018-10-29 11:29:45 +0530

    second commit

---
+++ RestifyAndroid/src/main/java/com/shaistech/restify/VolleySingleTon.java
@@ -0,0 +1,49 @@
+package com.shaistech.restify;
+
+/**
+ * Created by Syed Hussain Mehdi on 25-03-2018.
+ */
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+
+/**
+ * This class is used as singleton of Volley request
+ * */
+
+@SuppressWarnings("unchecked")
+public class VolleySingleTon {
+
+    private RequestQueue requestQueue;
+    private static Context mContext;
+    private static VolleySingleTon mInstance;
+
+    private VolleySingleTon(Context context){
+        mContext = context;
+        requestQueue = getRequestQueue();
+
+    }
+
+    private RequestQueue getRequestQueue(){
+        if(requestQueue==null){
+            requestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public static synchronized VolleySingleTon getInstance(Context context){
+        if(mInstance==null){
+            mInstance = new VolleySingleTon(context);
+        }
+        return mInstance;
+    }
+
+    public <T> void addToRequesQue(Request request){
+        requestQueue.add(request);
+    }
+}
+
---
+++ RestifyAndroid/src/main/java/com/shaistech/restify/Wrapper.java
@@ -0,0 +1,55 @@
+package com.shaistech.restify;
+
+import java.util.List;
+
+/**
+ * Created By Syed Hussain Medhi
+ * email : shmehdi01@gmail.com
+ * youtube : youtube.com/shmehdi5
+ * */
+
+@SuppressWarnings("unchecked")
+public class Wrapper<Model> {
+
+
+    private Wrapper.Status status;
+    private List<Model> data;
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public List<Model> getData() {
+        return data;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setData(List<Model> data) {
+        this.data = data;
+    }
+
+
+    public class Status {
+        private boolean success;
+        private String msg;
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public void setSuccess(boolean success) {
+            this.success = success;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+    }
+}
